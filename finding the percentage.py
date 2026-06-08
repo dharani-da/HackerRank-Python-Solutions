57961ec0ab318688486114c5c6694d7946d7e1cc
dharani-da <dharani28006@gmail.com>
2026-06-08 10:25:39 +0530

    Create finding the percentage.py

---
+++ finding the percentage.py
@@ -0,0 +1,15 @@
+if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+
+    for _ in range(n):
+        line = input().split()
+        name = line[0]
+        marks = list(map(float, line[1:]))
+        student_marks[name] = marks
+
+    query_name = input()
+    marks = student_marks[query_name]
+    average = sum(marks) / len(marks)
+
+    print(f"{average:.2f}")
